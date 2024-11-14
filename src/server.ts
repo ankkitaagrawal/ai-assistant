422557d3532b910a784aae02a5f942d8ea4962cd
walkover-ankit <ankitkumar@whozzat.com>
2024-11-14 15:12:38 +0530

    CORS bug fix

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,19 @@
 import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
-import chat from './route/chat'; 
+import chat from './route/chat';
+import cors from 'cors'; // Import the cors middleware
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+
 // Middleware to parse JSON
 app.use(express.json());
 
-app.use('/chat',chat );
+// Enable CORS
+app.use(cors());
+
+app.use('/chat', chat);
 
 // Define a route
 app.get('/', (req: Request, res: Response) => {
