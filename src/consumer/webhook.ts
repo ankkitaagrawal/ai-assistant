90334e7e54fc3cf187b766d8b3d68a5569a35a4d
tanish <tanish@whozzat.com>
2024-12-05 17:55:06 +0530

    added new system prompt for the cron

--- src/consumer/webhook.ts
+++ src/consumer/webhook.ts
@@ -1,29 +1,24 @@
 import { Channel } from '../config/rabbitmq';
 import logger from "../service/logger";
-import axios from 'axios';
 import producer from '../config/producer';
-import rtlayer from '../config/rtlayer';
 import { getCronDetailsById } from '../dbservices/cron';
-import { createMessage, sendMessage } from '../utility/aimiddleware';
-import { createOrFindThread } from '../dbservices/thread';
+import {  sendMessage } from '../utility/aimiddleware';
 
 const QUEUE_NAME = process.env.WEBHOOK_QUEUE || 'webhook';
-async function processMsg(message: any, channel: Channel) {
+async function processWebhook(message: any, channel: Channel) {
 
     try {
         const data = JSON.parse(message.content.toString());
-        
         const crondata = await getCronDetailsById(data.event);
         if (!crondata) throw new Error("invalid id")
         console.log(crondata,"crondata")
-        // const messageFromAI  = (await sendMessage("this is a message from the cron we have setup " , {context : crondata ,system_prompt : "below data is the reminder , that we have set up ,  now take action and drop a message to the user "} ))||"" 
-        let threadId = (crondata.to === crondata.from) ? crondata.to : await createOrFindThread(crondata.to, crondata.from);
-        await createMessage(threadId ,crondata?.message);
-        
-        rtlayer.message(message,{
-            channel : crondata?.to
-        });
+        await sendMessage( crondata.message ,
+        {
+            context : crondata ,
+            system_prompt : "The given message is result of an task schudled by you . Please take the appropriate action as per the user message  . Do not schedule any new tasks "}
+        )
         channel.ack(message);
+
     } catch (error: any) {
         producer.publishToQueue(QUEUE_NAME+"_FAILED",message)
         console.log(error?.response,"error", error)
@@ -35,6 +30,6 @@ async function processMsg(message: any, channel: Channel) {
 
 export default {
     queue: QUEUE_NAME,
-    processor: processMsg,
+    processor: processWebhook,
     batch: 1
 }
