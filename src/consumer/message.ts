99415fa114f46dacc1575c7c8822deebfc5abf13
walkover-ankit <ankitkumar@whozzat.com>
2024-12-10 15:48:42 +0530

    Bug fix

--- src/consumer/message.ts
+++ src/consumer/message.ts
@@ -3,26 +3,25 @@ import logger from "../service/logger";
 import producer from '../config/producer';
 import rtlayer from '../config/rtlayer';
 import { createMessage, } from '../utility/aimiddleware';
-import { createOrFindThread } from '../dbservices/thread';
 
 
 const QUEUE_NAME =  process.env.MESSAGE_QUEUE || 'message';
 async function processMsg(message: any, channel: Channel) {
-    try {
-        const data = JSON.parse(message.content.toString());
-        const {to , from, message : textMessage } = data 
-        let threadId = (to === from) ? to : await createOrFindThread(to, from);
-        await createMessage(threadId , textMessage); // need to write logic for message will  passed to assistant and then to human if needed 
-        rtlayer.message( JSON.stringify({ message : textMessage  , threadId : threadId } ) ,{
-            channel : to
-        });
-        channel.ack(message);
-    } catch (error: any) {
-        producer.publishToQueue(QUEUE_NAME+"_FAILED",message)
-        console.log(error?.response,"error", error)
-        logger.error(`[message] Error processing message: ${error.message}`);
-        channel.ack(message);
-    }
+    // try {
+    //     const data = JSON.parse(message.content.toString());
+    //     const {to , from, message : textMessage } = data 
+    //     let threadId = (to === from) ? to : await createOrFindThread(to, from);
+    //     await createMessage(threadId , textMessage); // need to write logic for message will  passed to assistant and then to human if needed 
+    //     rtlayer.message( JSON.stringify({ message : textMessage  , threadId : threadId } ) ,{
+    //         channel : to
+    //     });
+    //     channel.ack(message);
+    // } catch (error: any) {
+    //     producer.publishToQueue(QUEUE_NAME+"_FAILED",message)
+    //     console.log(error?.response,"error", error)
+    //     logger.error(`[message] Error processing message: ${error.message}`);
+    //     channel.ack(message);
+    // }
 
 }
 
