4fcf7d6462682f148713edf132e6a1279df8a436
Ankit Kumar <104899126+walkover-ankit@users.noreply.github.com>
2024-12-23 20:43:44 +0530

    Bug Fix

--- src/job/data_source.ts
+++ src/job/data_source.ts
@@ -147,8 +147,8 @@ class PublishEvent extends Transform {
                             resourceId: fullDocument._id?.toString(),
                             url: url,
                             meta: {
-                                domain: fullDocument.metaData?.domain,
-                                extension: fullDocument.metaData?.extension
+                                domain: request.metaData?.domain,
+                                extension: request.metaData?.extension
                             },
                             timestamp: Date.now()
                         }
@@ -163,8 +163,8 @@ class PublishEvent extends Transform {
                             content: fullDocument.content,
                             public: fullDocument.public,
                             meta: {
-                                domain: request.metaData.domain,
-                                extension: request.metaData.extension
+                                domain: request.metaData?.domain,
+                                extension: request.metaData?.extension
                             },
                             timestamp: Date.now()
                         }
