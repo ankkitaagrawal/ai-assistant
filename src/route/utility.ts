4bf92c1da0b2351959673b5b81e529051a761edc
walkover-ankit <ankitkumar@whozzat.com>
2024-12-24 00:01:32 +0530

    Added utility for testing loader/chunker

--- src/route/utility.ts
+++ src/route/utility.ts
@@ -2,6 +2,11 @@ import { deleteCron, getCronDetailsOfUser, saveCronJobData, updateUserPrompt } f
 import { Request, Response } from "express";
 import redis from '../config/redis';
 import { APIResponseBuilder } from "../service/utility";
+import { DocumentLoader } from "../service/document-loader";
+import { Doc } from '../service/document';
+import { next } from "cheerio/dist/commonjs/api/traversing";
+import { NextFunction } from "connect";
+
 const express = require('express');
 
 const routes = express.Router();
@@ -18,6 +23,22 @@ routes.route('/cache/flush').delete(async (req: Request, res: Response) => {
     }
     const response = responseBuilder.setSuccess({ "keys": keys }).build();
     res.json(response);
+});
+
+routes.route('/chunk').get(async (req: Request, res: Response, next: NextFunction) => {
+    try {
+
+        const url = req.query.url as string;
+        const chunkSize = parseInt(req.query.chunkSize as string) || 512;
+        const overlap = parseInt(req.query.overlap as string) || 50;
+        const loader = new DocumentLoader();
+        const content = await loader.getContent(url) as any;
+        let doc = new Doc("testing", content, { agentId: "testing", public: true });
+        doc = await doc.chunk(chunkSize, overlap);
+        res.json({ doc });
+    } catch (error) {
+        next(error);
+    }
 });
 
 export default routes;
