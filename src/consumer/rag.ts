195047f1a11fdda69a9b1acf0d11f62e183f113f
Ankit <ankitkumar@whozzat.com>
2025-01-02 18:34:00 +0530

    A description will be added to each resource to help llm decide when to use it

--- src/consumer/rag.ts
+++ src/consumer/rag.ts
@@ -8,6 +8,8 @@ import { DocumentLoader } from '../service/document-loader';
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 import ResourceService from '../dbservices/resource';
 import { Doc, MongoStorage, OpenAiEncoder, PineconeStorage } from '../service/document';
+import { AIMiddlewareBuilder } from '../utility/aimiddleware';
+import env from '../config/env';
 
 
 const QUEUE_NAME = process.env.RAG_QUEUE || 'rag';
@@ -18,18 +20,19 @@ async function processMsg(message: any, channel: Channel) {
         const { version, event, data } = EventSchema.parse(msg);
         resourceId = data.resourceId;
         console.log(`Event: ${event}`);
+        let pipelineStatus = null;
         switch (event) {
             case 'load':
                 const loader = new DocumentLoader();
                 const content = await loader.getContent(data.url);
                 await ResourceService.updateResource(data.resourceId, { content });
-                ResourceService.updateMetadata(data.resourceId, { status: 'loaded' }).catch(error => console.log(error));
+                pipelineStatus = "loaded";
                 break;
             case 'delete': {
                 const doc = new Doc(data.resourceId);
                 await doc.delete(new PineconeStorage()); // WARNING: Pinecone delete is dependent on id from mongo
                 await doc.delete(new MongoStorage());
-                ResourceService.updateMetadata(data.resourceId, { status: 'deleted' }).catch(error => console.log(error));
+                pipelineStatus = "deleted";
                 break;
             }
             case 'chunk': {
@@ -43,18 +46,26 @@ async function processMsg(message: any, channel: Channel) {
                     chunk.delete(new MongoStorage());
                     throw error;
                 }
-                ResourceService.updateMetadata(data.resourceId, { status: 'chunked' }).catch(error => console.log(error));
+                await updateDescription(data?.resourceId, data?.content).catch(error => logger.error(error));
+                pipelineStatus = "chunked";
                 break;
             }
             case 'update':
-                // TODO: Change the visibility of the resource
-                break;
+                {
+                    // TODO: Change the visibility of the resource
+                    break;
+                }
             default:
-                logger.error(`[message] Unknown event type: ${event}`);
-                throw new Error(`Unknown event type: ${event}`);
-                break;
+                {
+                    logger.error(`[message] Unknown event type: ${event}`);
+                    throw new Error(`Unknown event type: ${event}`);
+                    break;
+                }
         }
-
+        if (pipelineStatus) {
+            await ResourceService.updateMetadata(data.resourceId, { status: pipelineStatus }).catch(error => console.log(error));
+            await rtlayer.message(JSON.stringify({ id: data?.resourceId, status: pipelineStatus }), { channel: "resource" }).catch(error => logger.error(error));
+        }
         channel.ack(message);
     } catch (error: any) {
         console.log(error);
@@ -71,4 +82,13 @@ export default {
     queue: QUEUE_NAME,
     processor: processMsg,
     batch: 1
+}
+
+
+async function updateDescription(resourceId: string, content: string) {
+    // Generate description
+    const aiMiddleware = new AIMiddlewareBuilder(env.AI_MIDDLEWARE_AUTH_KEY);
+    const descriptionGenerator = aiMiddleware.useBridge("676febfe9768bd87271d3e3e").useOpenAI("gpt-4o-mini").build();
+    const description = await descriptionGenerator.sendMessage(content);
+    await ResourceService.updateResource(resourceId, { description });
 }
