a9d25f0e8b66ce7cd40aa46d98598253635477c9
walkover-ankit <ankitkumar@whozzat.com>
2025-01-07 22:06:56 +0530

    Created zod schema and type for fallback tool

--- src/type/event.ts
+++ src/type/event.ts
@@ -24,10 +24,18 @@ export const updateDiarySchema = z.object({
 });
 export type UpdateDiary = z.infer<typeof updateDiarySchema>;
 
-
-
+export const fallbackSchema = z.object({
+    event: z.literal('fallback'),
+    data: z.object({
+        agentId: z.string().describe("The ID of the agent"),
+        message: z.string().describe("The message for which the fallback is triggered"),
+        threadId: z.string().describe("The ID of thread in which message was sent"),
+        userId: z.string().describe("The ID of user who sent the message"),
+    })
+})
+export type Fallback = z.infer<typeof fallbackSchema>;
 
 export const EventSchema = z.discriminatedUnion('event', [
-    generateThreadNameSchema, updateDiarySchema
+    generateThreadNameSchema, updateDiarySchema, fallbackSchema
 ]);
 export type Event = z.infer<typeof EventSchema>;
