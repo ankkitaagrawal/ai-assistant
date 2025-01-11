5cac6915797cbf8d40d181507d0551aeb69a45aa
walkover-ankit <ankitkumar@whozzat.com>
2025-01-11 20:36:41 +0530

    Added method to get user from proxy

--- src/utility/channel.ts
+++ src/utility/channel.ts
@@ -72,5 +72,28 @@ export const getUser = async (uid: string) => {
     const { data: responseData, status, statusCode } = await axios.post(`https://delve-api.intospace.io/search/prod-space?query=${uid}&API_KEY=${env.CHANNEL_AUTHKEY}&size=1`, data);
     // TODO: Handle status codes
     return responseData.hits?.hits[0]?._source;
+}
+
+export type ProxyUser = {
+    id: number,
+    name: string,
+    mobile: string,
+    email: string,
+    is_block: boolean,
+}
+export const getProxyUser = async (proxyUid: string): Promise<ProxyUser> => {
+    const axios = require('axios');
+
+    let config = {
+        method: 'get',
+        maxBodyLength: Infinity,
+        url: `https://routes.msg91.com/api/870623b1736406370677f756255301/getDetails?user_id=${proxyUid}`,
+        headers: {
+            'Authkey': env.PROXY_API_KEY
+        }
+    };
+
+    const { data: responseData, status, statusCode } = await axios.request(config);
+    return responseData?.data?.data[0];
 
 }
